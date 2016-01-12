fd0f9ac5497569c2adc1c4beaf9fe2f3c4bdb055
Stery <stery007@gmail.com>
2016-01-12 22:13:59 +0200

    small tweaks

--- 2048.c
+++ 2048.c
@@ -727,44 +727,7 @@ void doundo(undo *v, int **b, int *scor)
       }
 }
 
-void printbeforewin(WINDOW *fereastra);
-/*{
-      int x,y,maxy,i;
-      wclear(fereastra);
-      getmaxyx(fereastra,maxy,i);
-      start_color();
-      init_pair(1337,COLOR_GREEN,COLOR_BLACK);
-      wattron(fereastra, COLOR_PAIR(1337) | A_BLINK | A_BOLD);
-      for(i=maxy-1;i>1;i--)
-      {
-            mvwprintw(fereastra,i,0,r1);
-            mvwprintw(fereastra,i+1,0,r2);
-            mvwprintw(fereastra,i+2,0,r3);
-            mvwprintw(fereastra,i+3,0,r4);
-            mvwprintw(fereastra,i+4,0,r5);
-            mvwprintw(fereastra,i+5,0,r6);
-            mvwprintw(fereastra,i+6,0,r7);
-            mvwprintw(fereastra,i+7,0,r8);
-            mvwprintw(fereastra,i+8,0,r9);
-            mvwprintw(fereastra,i+9,0,r10);
-            mvwprintw(fereastra,i+10,0,r11);
-            mvwprintw(fereastra,i+11,0,r12);
-            mvwprintw(fereastra,i+12,0,r13);
-            mvwprintw(fereastra,i+13,0,r14);
-            mvwprintw(fereastra,i+14,0,r15);
-            mvwprintw(fereastra,i+15,0,r16);
-            mvwprintw(fereastra,i+16,0,r17);
-            mvwprintw(fereastra,i+17,0,r18);
-            mvwprintw(fereastra,i+18,0,r19);
-            mvwprintw(fereastra,i+19,0,r20);
-            mvwprintw(fereastra,i+20,0,r21);
-            wrefresh(fereastra);
-            sleep(0.2);
-      }
-      wattroff(fereastra, COLOR_PAIR(1337) | A_BLINK | A_BOLD);
-      sleep(2);
-}*/
-
+//void printbeforewin(WINDOW *fereastra);
 
 int main()
 {
